71c4b9875a997a141c90895067c6d57842e9a6d0
Michael Latman <michael@latman.org>
2014-12-21 17:00:48 -0500

    Initial Commit

---
+++ StudyBranch/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  StudyBranch
+//
+//  Created by Michael Latman on 12/21/14.
+//  Copyright (c) 2014 Michael Latman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ StudyBranch/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  StudyBranch
+//
+//  Created by Michael Latman on 12/21/14.
+//  Copyright (c) 2014 Michael Latman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
